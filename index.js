5af69a7ddb506cc4be2aad6a96217e11f2f8e420
Jari Kasandiredjo <jarikasan@gmail.com>
2022-01-19 11:05:54 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-$('th').click(function(){
-    var table = $(this).parents('.models').eq(0)
-    var rows = table.find('.bodyRows:gt(0)').toArray().sort(comparer($(this).index()))
-    this.asc = !this.asc
-    if (!this.asc){rows = rows.reverse()}
-    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-})
+// Sorting
+$('th').click(function() {
+    const table = $(this).parents('.models').eq(0);
+    const rows = table.find('.bodyRows:gt(0)').toArray().sort(comparer($(this).index()));
+    this.asc = !this.asc;
+    if (!this.asc) { rows = rows.reverse(); }
+    for (let i = 0; i < rows.length; i++) { table.append(rows[i]); }
+});
+
 function comparer(index) {
     return function(a, b) {
-        var valA = getCellValue(a, index), valB = getCellValue(b, index)
-        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        const valA = getCellValue(a, index), valB = getCellValue(b, index);
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
     }
 }
-function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
 
-// {
-//     "id": 22926,
-//     "brand": "Apple",
-//     "model": "iPhone X",
-//     "os": "iOS",
-//     "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/IPhone_X_vector.svg/440px-IPhone_X_vector.svg.png",
-//     "screensize": 5
-// }
+function getCellValue(row, index){ return $(row).children('td').eq(index).text(); }
 
+// Fetch model table data on page load
 $(function() {
     const tableBody = $('.models').find('tbody');
 
@@ -31,7 +26,7 @@ $(function() {
      })
      .done(function(data) {
          data.forEach(model => {
-             tableBody.append(`<tr class="bodyRows"><td>${model.brand}</td><td>${model.model}</td><td>${model.os}</td><td>${model.screensize}</td><td><img alt="${model.brand} ${model.model}" src="${model.image}" class="phone-img"></td></tr>`)
+             tableBody.append(`<tr class="bodyRows"><td>${model.brand}</td><td>${model.model}</td><td>${model.os}</td><td>${model.screensize}</td><td><img alt="${model.brand} ${model.model}" src="${model.image}" class="phone-img"></td></tr>`);
          });
      });
 });
@@ -49,24 +44,8 @@ $('#model-form').submit(function(e) {
                 url: data.URI,
                 method: "GET",
             }).done(function(data) {
-                tableBody.append(`<tr class="bodyRows"><td>${data.brand}</td><td>${data.model}</td><td>${data.os}</td><td>${data.screensize}</td><td><img alt="${data.brand} ${data.model}" src="${data.image}" class="phone-img"></td></tr>`)
-            })
+                tableBody.append(`<tr class="bodyRows"><td>${data.brand}</td><td>${data.model}</td><td>${data.os}</td><td>${data.screensize}</td><td><img alt="${data.brand} ${data.model}" src="${data.image}" class="phone-img"></td></tr>`);
+            });
         }
     });
 });
-
-/*
-
-var mylist = $('#myUL');
-var listitems = mylist.children('li').get();
-listitems.sort(function(a, b) {
-   return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
-})
-$.each(listitems, function(idx, itm) { mylist.append(itm); });
-
-
-
-
-*/
-
-
