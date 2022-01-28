ceb81f756f9ab5b1dd4a5cc68c63cba85b1b8b55
Jari Kasandiredjo <jarikasan@gmail.com>
2022-01-28 15:17:27 +0100

    fix routes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.get('/items', function(req, res) {
     db.all('SELECT * FROM phones', function(err, rows) {
         if (err) {
-            return res.status(500).send(error);
+            return res.status(500).send(err);
         } else {
             return res.json(rows);
         }
@@ -23,42 +23,22 @@ app.get('/items', function(req, res) {
 });
 
 app.post('/items', function(req, res) {
-    
-    
-    const _body = req.body
-    _body.brand = req.body.brand,
-    _body.model = req.body.model,
-    _body.os = req.body.os,
-    _body.image = req.body.image,
-    _body.screensize = req.body.screensize
-      
+    const item = {
+        brand: req.body.topic,
+        model: req.body.date,
+        os: req.body.body,
+        image: req.body.images,
+        screensize: req.body.files
+      };
 
     db.run(`INSERT INTO phones (brand, model, os, image, screensize) VALUES (?, ?, ?, ?, ?)`,
-    [_body], function(err) {
-      if (err) {
+    [item.brand, item.model, item.os, item.image, item.screensize], function(err) {
+        if (err) {
             return res.status(500).send(err);
         } else {
             return res.status(200).send();
         }
     });
-  
-    
-     //db.close();
-  
-});
-
-app.get('/db-example', function(req, res) {
-    db.all(`SELECT * FROM phones WHERE id=?`, ['Fairphone'], function(err, rows) {
-
-    	// # Return db response as JSON
-    	return res.json(rows)
-    });
-});
-
-app.post('/post-example', function(req, res) {
-	// This is just to check if there is any data posted in the body of the HTTP request:
-	console.log(req.body);
-	return res.json(req.body);
 });
 
 const port = process.env.PORT || 3000;
