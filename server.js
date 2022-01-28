f3371503f81898262d8eb0e3ff19f3db4501fee0
francomarcon <francomarcon2001@gmail.com>
2022-01-28 14:56:04 +0100

    post

--- server.js
+++ server.js
@@ -22,7 +22,27 @@ app.get("/items", function(req, res) {
 });
 
 app.post("/items", function(req, res) {
-    db.all()
+    
+    const post = {
+        brand: req.body.topic,
+        model: req.body.date,
+        os: req.body.body,
+        image: req.body.images,
+        screensize: req.body.files
+      };
+
+    db.run(`INSERT INTO phones (brand, model, os, image, screensize) VALUES (?, ?, ?, ?, ?)`,
+    [brand, model, os, image, screensize], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      
+      console.log(`A row has been inserted`);
+    });
+  
+    
+     //db.close();
+  
 });
 
 // This route responds to http://localhost:3000/db-example by selecting some data from the
